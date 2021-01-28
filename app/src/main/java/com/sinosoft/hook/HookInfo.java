234ab977b04b4ea6fdf92820762be49ea7555b01
wiky <wiky19901217@gmail.com>
2021-01-28 16:00:42 +0800

    no message

--- app/src/main/java/com/sinosoft/hook/HookInfo.java
+++ app/src/main/java/com/sinosoft/hook/HookInfo.java
@@ -23,35 +23,6 @@ public class HookInfo {
     //对象指定的包名+类名
     public static final String hookClassPath_Contact=hookPackage+hookClassNameBypackage_Contact+hookClassName_Contact;
     //对象的方法名：
-    public static final String hookMethod_GetCacheOneContactGroupInfoUsers_Contact="GetCacheOneContactGroupInfoUsers";
-
-    //hook对象的类名
-    public static final String hookClassName_ConversationService="ConversationService";
-    //对象的所在包
-    public static final String hookClassNameBypackage_ConversationService=".foundation.logic.";
-    //对象指定的包名+类名
-    public static final String hookClassPath_ConversationService=hookPackage+hookClassNameBypackage_ConversationService+hookClassName_ConversationService;
-    //对象的方法名：
-    public static final String hookMethod_SendMessage_ConversationService="SendMessage";
-
-
-    //hook对象的类名
-    public static final String hookClassName_Conversation="Conversation";
-    //对象的所在包
-    public static final String hookClassNameBypackage_Conversation=".foundation.model.";
-    //对象指定的包名+类名
-    public static final String hookClassPath_Conversation=hookPackage+hookClassNameBypackage_Conversation+hookClassName_Conversation;
-    //对象的方法名：
-    public static final String hookMethod_GetUserList_Conversation="GetUserList";
-    public static final String hookMethod_onAddMessages_Conversation="onAddMessages";
-
-    //hook对象的类名
-    public static final String hookClassName_CrmRoomService="CrmRoomService";
-    //对象的所在包
-    public static final String hookClassNameBypackage_CrmRoomService=".foundation.logic.";
-    //对象指定的包名+类名
-    public static final String hookClassPath_CrmRoomService=hookPackage+hookClassNameBypackage_CrmRoomService+hookClassName_CrmRoomService;
-    //对象的方法名：
-    public static final String hookMethod_GetUserList_CrmRoomService="";
+    public static final String hookMethod_GetCacheOneContactGroupInfoUsers_Contact="a";
 
 }
